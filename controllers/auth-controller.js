50939e937a5030f4f58159c999699bd8ba84536e
seasittipol <charmy_best@hotmail.com>
2024-01-29 15:06:30 +0700

    destructor confirmPassword

--- controllers/auth-controller.js
+++ controllers/auth-controller.js
@@ -1,3 +1,4 @@
+const bcrypt = require('bcrypt')
 const db = require('../models/db')
 
 // module.exports.register == exports.register จะ path module ให้
@@ -5,12 +6,15 @@ exports.register = async (req, res, next) => {
     const { s_code, password, confirmPassword, firstname, email } = req.body
     // validation : joi
     try {
-        if (!(s_code && password && confirmPassword && firstname && email)) {
+        if (!(s_code && password && confirmPassword && firstname)) {
             return next(new Error("fullfill blank input::400"))
         }
         if (password !== confirmPassword) {
             throw new Error("check confirm password::401")
         }
+        const { confirmPassword: cfpw, ...data } = req.body
+        console.log(data)
+
         res.json(req.body)
     } catch (err) {
         next(err)
