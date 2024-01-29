7415c09d09335d9563955c2c01702326e15f4d38
seasittipol <charmy_best@hotmail.com>
2024-01-29 15:13:16 +0700

    create register

--- controllers/auth-controller.js
+++ controllers/auth-controller.js
@@ -13,9 +13,11 @@ exports.register = async (req, res, next) => {
             throw new Error("check confirm password::401")
         }
         const { confirmPassword: cfpw, ...data } = req.body
+        data.password = await bcrypt.hash(data.password, 10)
         console.log(data)
-
-        res.json(req.body)
+        const newStudent = await db.student.create({ data: data })
+        console.log(newStudent)
+        res.json({ msg: 'Register Success' })
     } catch (err) {
         next(err)
     }
